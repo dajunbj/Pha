565ac2de03b0130afb821e1b0798d89d2c64b84f
dajunbj <dajunbj@gmail.com>
2016-02-20 23:07:23 +0900

    upd

--- src/main/resources/sqlfile/GoodProducerDao_selectValueLabel.sql
+++ src/main/resources/sqlfile/GoodProducerDao_selectValueLabel.sql
@@ -1,7 +1,7 @@
-select type_id as value,tp_nm as label from good_type
+select good_producer_id as value,good_producer_nm as label from good_producer
 where del_flg='0'
 
 /*IF param.type_id != null*/
     and type_id = /*param.type_id*/
 /*END*/
-order by type_id
+order by good_producer_id
