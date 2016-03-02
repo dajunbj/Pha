be837c46b757c58a496415e078e7d0770db040b9
dajunbj <dajunbj@gmail.com>
2016-03-03 00:13:25 +0900

    q

--- src/main/resources/sqlfile/GoodsDao_getGoodsList.sql
+++ src/main/resources/sqlfile/GoodsDao_getGoodsList.sql
@@ -1,6 +1,12 @@
 select 
-*
-FROM goods
+T1.*,
+T2.good_producer_nm,
+T3.tp_nm
+FROM goods T1
+left join good_producer T2 on 
+T1.good_producer_id = T2.good_producer_id
+left join good_type T3 on 
+T1.type_id = T3.type_id
 /*IF param != null*/
 where 
     0=0
@@ -10,6 +16,6 @@ where
 	/*IF param.good_producer_id != null*/
 	and good_producer_id = /*param.good_producer_id*/
 	/*END*/
-    and del_flg='0'
+    and T1.del_flg='0'
 /*END*/
 order by goods_id
